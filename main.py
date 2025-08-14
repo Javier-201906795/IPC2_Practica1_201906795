7974b194a04407bebc52053200f57b4dae478cae
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-08-14 17:22:35 -0600

    comentar lineas sin uso

--- main.py
+++ main.py
@@ -12,38 +12,41 @@ import random
 print("Practica 1")
 print("nuevo libro")
 Libro1 = LibFisico("El principito", "Desconocido",  1)
-print(Libro1.gettitulolibro())
-print(Libro1.gettitulo())
-print(Libro1.getID())
 
 
-print(Libro1.getestado())
 
-print(Libro1.prestar())
-
-print(Libro1.getestado())
-print(Libro1.prestar())
-
-print(Libro1.devolver())
-
-print(Libro1.getestado())
-
-print(Libro1.devolver())
-
-print(Libro1.mostrarinfo())
-
-print(Libro1.prestarLibro(1,5))
-print(Libro1.getestado())
-print(Libro1.devolverLibro(1,10))
-
-print(Libro1.prestarLibro(1,29))
-print(Libro1.getestado())
-print(Libro1.devolverLibro(2,1))
+# print(Libro1.gettitulolibro())
+# print(Libro1.gettitulo())
+# print(Libro1.getID())
 
 
-print(Libro1.prestarLibro(1,30))
-print(Libro1.getestado())
-print(Libro1.devolverLibro(2,15))
+# print(Libro1.getestado())
+
+# print(Libro1.prestar())
+
+# print(Libro1.getestado())
+# print(Libro1.prestar())
+
+# print(Libro1.devolver())
+
+# print(Libro1.getestado())
+
+# print(Libro1.devolver())
+
+# print(Libro1.mostrarinfo())
+
+# print(Libro1.prestarLibro(1,5))
+# print(Libro1.getestado())
+# print(Libro1.devolverLibro(1,10))
+
+# print(Libro1.prestarLibro(1,29))
+# print(Libro1.getestado())
+# print(Libro1.devolverLibro(2,1))
+
+
+# print(Libro1.prestarLibro(1,30))
+# print(Libro1.getestado())
+# print(Libro1.devolverLibro(2,15))
 
 
 
